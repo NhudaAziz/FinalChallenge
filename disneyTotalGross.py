f592156feaac3f1cacf8b6e6d9da0f33d862b2dc
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-09 15:36:52 +0800

    Update disneyTotalGross.py

--- disneyTotalGross.py
+++ disneyTotalGross.py
@@ -3,16 +3,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
+
+st.header("My first Streamlit App")
+st.title("This is a title")
 
 st.write("""
-# Disney Movies Gross Income
-This is the total gross income for the Disney movies from 1937 - 2016
+# This is a first-level heading
+## This is a second-level heading
 """)
 
-gross = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/FinalChallenge/main/disney_movies_total_gross.csv")
-feature_cols = ['movie_title','release_date','genre','mpaa_rating']
-# X = gross[feature_cols]
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
+
+# st.write("""
+# # Disney Movies Gross Income
+# This is the total gross income for the Disney movies from 1937 - 2016
+# """)
+
+# gross = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/FinalChallenge/main/disney_movies_total_gross.csv")
+# feature_cols = ['movie_title','release_date','genre','mpaa_rating']
+# # X = gross[feature_cols]
 # Y = gross.inflation_adjusted_gross
 
 # chart_data = pd.DataFrame(
