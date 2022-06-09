a552678e04e11c1f5f5f2c39c626764b97012a4a
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-09 15:35:37 +0800

    Update disneyTotalGross.py

--- disneyTotalGross.py
+++ disneyTotalGross.py
@@ -7,17 +7,17 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Disney Movies Gross Income
-
+This is the total gross income for the Disney movies from 1937 - 2016
 """)
 
 gross = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/FinalChallenge/main/disney_movies_total_gross.csv")
 feature_cols = ['movie_title','release_date','genre','mpaa_rating']
-X = gross[feature_cols]
-Y = gross.inflation_adjusted_gross
+# X = gross[feature_cols]
+# Y = gross.inflation_adjusted_gross
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3), #20 rows 3 columns
-     columns=['a', 'b', 'c'])
+# chart_data = pd.DataFrame(
+#      np.random.randn(20, 3), #20 rows 3 columns
+#      columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+# st.line_chart(chart_data)
 
