7be5b7b11edcc39a032eb451245c896082142dfc
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-13 15:09:49 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-st.header("My first Streamlit App")
+st.header("Final Challenge")
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3), #20 rows 3 columns
