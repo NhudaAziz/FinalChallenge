32eea2fc81e1a938eba32fcbb7e8abc10b809e35
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-16 00:07:40 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import geopy
-
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
+import plotly.express as px
 
 st.header("Best Work-Life Balance Cities in 2022")
 
 df = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/FinalChallenge/main/Cities%20with%20the%20Best%20Work-Life%20Balance%202022.csv")
+
+df = get_data()
 
 # ---- Sidebar ----
 st.sidebar.header("Choose Filter:")
@@ -28,15 +27,5 @@ df_selection = df.query(
     "Country == @country & City == @city"
 )
 
-geolocator = Nominatim(user_agent="GTA Lookup")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-location = geolocator.geocode(country+", "+city)
 
-lat = location.latitude
-lon = location.longitude
-
-map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
-
-st.map(map_data)
-
-#st.line_chart(chart_data)
+st.line_chart(chart_data)
