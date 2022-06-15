327fa376b9795dd8676030df2d628dec8d2dee90
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-15 23:52:54 +0800

    Update final.py

--- final.py
+++ final.py
@@ -26,7 +26,7 @@ df_selection = df.query(
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon']))
+    columns=['lat', 'lon'])
 
 st.map(map_data)
 
