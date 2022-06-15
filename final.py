a6f8451a7fb94f8539d8307b4cafef2a810db8e9
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-15 22:40:28 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,28 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-st.header("Final Challenge")
+
+st.header("Best Work-Life Balance Cities in 2022")
+
+df = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/FinalChallenge/main/Cities%20with%20the%20Best%20Work-Life%20Balance%202022.csv")
+
+# ---- Sidebar ----
+st.sidebar.header("Choose Filter:")
+country = st.sidebar.multiselect(
+    "Select the Country:",
+    options=df["Country"].unique(),
+    default=df["Country"].unique()
+)
+
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+df_selection = df.query(
+    "Country == @country & City == @city"
+)
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3), #20 rows 3 columns
