2d760568f2a59a6764e169824ed8f560b18b6e50
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-15 23:51:44 +0800

    Update final.py

--- final.py
+++ final.py
@@ -24,7 +24,9 @@ df_selection = df.query(
     "Country == @country & City == @city"
 )
 
-map_data = pd.DataFrame(df)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']))
 
 st.map(map_data)
 
